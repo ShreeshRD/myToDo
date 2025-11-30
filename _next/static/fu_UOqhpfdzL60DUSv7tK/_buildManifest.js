914d6d403969743f65f33e5c16134a76772939b5
ShreeshRD <shreeshrdevi2@gmail.com>
2025-11-30 09:30:09 +0530

    Updates

---
+++ _next/static/fu_UOqhpfdzL60DUSv7tK/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/myToDo//_next/:path+",
+        "destination": "/myToDo/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
