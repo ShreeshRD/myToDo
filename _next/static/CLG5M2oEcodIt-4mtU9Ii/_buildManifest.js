d0de1ad8af309ce1c15d9946d2986cbacc6c2b4a
ShreeshRD <shreeshrdevi2@gmail.com>
2026-01-01 12:07:24 +0000

    Updates

--- _next/static/CLG5M2oEcodIt-4mtU9Ii/_buildManifest.js
+++ _next/static/CLG5M2oEcodIt-4mtU9Ii/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/myToDo//_next/:path+",
-        "destination": "/myToDo/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
